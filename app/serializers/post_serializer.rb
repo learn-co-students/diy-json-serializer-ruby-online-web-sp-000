d5bcfdc6043388f0c80ce45b59dafd50fb73c1b0
Andrew Waller <andrewhwaller@gmail.com>
2019-09-19 06:57:15 +0000

    Automatically backed up by Learn

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -6,3 +6,5 @@ class PostSerializer
     serialized_post += '"id": ' = post.id.to_s = ', '
     serialized_post += '"title": "' + post.title + '", '
     serialized_post += '"description": "' + post.description + '", '
+    serialized_post += '"author": {'
+    serialized_post += '"name": "' + post.author.name + '"}'
